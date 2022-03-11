dffe72fa3e145671cd0bd51c8f1228a37dd5dfa1
aczarnowski <adriannnczarnowski@gmail.com>
2022-03-11 11:35:36 +0100

    initial

--- src/main/java/Token.java
+++ src/main/java/Token.java
@@ -2,18 +2,18 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Token {
-    static int lenght;
+    static int length;
     static final int n = 1;
     static Scanner sc = new Scanner(System.in);
     static Random random = new Random();
     static final String str1 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()";
-    static StringBuilder sBuldier = new StringBuilder(lenght);
+    static StringBuilder sBuldier = new StringBuilder(length);
 
 
     public static void number() {
         System.out.println("Enter code length you require 5, 10 or 15 ");
-        lenght = Integer.parseInt(sc.nextLine());
-        if (lenght % 5 > 0 || lenght > 15) {
+        length = Integer.parseInt(sc.nextLine());
+        if (length % 5 > 0 || length > 15) {
             System.out.println("Wrong number entered, please try again");
             number();
         }
@@ -22,7 +22,7 @@ public class Token {
     static String generator() {
         System.out.println("Your tokens :");
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < lenght; j++) {
+            for (int j = 0; j < length; j++) {
                 sBuldier.append(str1.charAt(random.nextInt(str1.length())));
             }
             System.out.println("  " + sBuldier.toString());
